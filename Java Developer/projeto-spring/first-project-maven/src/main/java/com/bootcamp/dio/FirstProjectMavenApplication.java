6661a3bcaa1967522f96418f4e8d4736ec26b9de
unknown <vrnakano@gmail.com>
2021-01-10 14:58:38 -0300

    Criação de novo projeto Maven

---
+++ Java Developer/projeto-spring/first-project-maven/src/main/java/com/bootcamp/dio/FirstProjectMavenApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectMavenApplication.class, args);
+	}
+
+}
