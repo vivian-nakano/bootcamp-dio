e1958f636718fc5dee2146c6d3def5a830f3f9b3
unknown <vrnakano@gmail.com>
2021-02-01 23:20:28 -0300

    Instalação da dependência Lombok

--- project-api/src/main/java/com/project/api/model/Cliente.java
+++ project-api/src/main/java/com/project/api/model/Cliente.java
@@ -1,9 +1,21 @@
 package com.project.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
 	
+	@Id //chave primária
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
 	
+	@Column (nullable = false)
+	private String nome;
 
+
+	
 }
