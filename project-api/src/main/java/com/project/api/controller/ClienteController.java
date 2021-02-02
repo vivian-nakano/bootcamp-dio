befb942bbd5e00c34fa4aa3009832858fcb8fb46
unknown <vrnakano@gmail.com>
2021-02-01 23:33:50 -0300

    Inserção de regras application properties

--- project-api/src/main/java/com/project/api/controller/ClienteController.java
+++ project-api/src/main/java/com/project/api/controller/ClienteController.java
@@ -2,20 +2,32 @@ package com.project.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.api.model.Cliente;
+import com.project.api.repository.ClienteRepository;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	//método
 	@GetMapping
 	public List<Cliente> listar() {
+		return clienteRepository.findAll();
 		
+	}
+	
+	@PostMapping
+	public Cliente adicionar(Cliente cliente) {
+		return clienteRepository.save(cliente);
 	}
 
 }
--- project-api/src/main/java/com/project/api/model/Cliente.java
+++ project-api/src/main/java/com/project/api/model/Cliente.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
+@Data
 @Entity
 public class Cliente {
 	
